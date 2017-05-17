3fe0ab70bf0d1d7ab9f3ad0ce8566f4128337d25
孤独的蔬菜 <shuai19970217@163.com>
2017-05-17 09:35:03 +0800

    数组的操作

--- 5.17_lx/Program.cs
+++ 5.17_lx/Program.cs
@@ -10,6 +10,42 @@ namespace _5._17_lx
     {
         static void Main(string[] args)
         {
+            #region 冒泡排序
+
+            //int[] number = new int[10]{20,5,17,28,19,15,29,87,65,32 };
+
+            //for (int i = 0; i < number.Length-1; i++)
+            //{
+            //    for (int j = 0; j <number.Length-1-i; j++)
+            //    {
+            //        if (number[j]<number[j+1])
+            //        {
+            //            int temp = number[j];
+            //            number[j] = number[j + 1];
+            //            number[j + 1] = temp;
+            //        }
+            //    }
+            //}
+
+            #endregion
+
+
+            #region 数组倒叙
+
+            int[] arr = { 16,25,9,90,23};
+            for (int i = 0; i <arr.Length/2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length-1-i];
+                arr[arr.Length - 1 - i] = temp;
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item +" ");
+            }
+            Console.ReadKey();
+
+            #endregion
         }
     }
 }
